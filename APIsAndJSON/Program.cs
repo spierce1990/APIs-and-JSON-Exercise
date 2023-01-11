980dea1cd641faf4250ad7165008ea9f50d6e7be
Sionylen <sionylenpierce@outlook.com>
2023-01-11 01:55:02 -0600

    added

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace APIsAndJSON
-{
-    public class Program
+﻿namespace APIsAndJSON;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           
+            var client = new HttpClient();
+
+            var kanyeURL = "https://api.kanye.rest/";
+
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+             string kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+
+        string ronResponse = client.GetStringAsync(ronURL).Result;
+        //create end point
+        //parse object
+
+        var kanyeObject = JObject.Parse(kanyeResponse);
+
+        var ronObject = JArray.Parse(ronResponse);
+             //method json
+
+            //write it out
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("------------------------");
+                Console.WriteLine($"Kanye: {kanyeObject["quote"]}");
+
+                Console.WriteLine($"Ron Swanson: {ronObject[0]}");
+
+            }
         }
     }
-}
+
